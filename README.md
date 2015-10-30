db2c2ded716830bdaa7508737497b622f4aa101b
al13n <akshitpoddar@gmail.com>
2015-10-30 00:02:00 -0400

    Code runs

--- README.md
+++
@@ -1,2 +0,0 @@
-# ftp-over-udp
-Fall'15, Network Programming Assignment 2
--- as2/unprtt.h
+++ as2/unprtt.h
@@ -3,15 +3,12 @@
 
 #include    "unp.h"
 
-struct rtt_info {
-    // Assignment 2 Change
-    
+struct rtt_info {    
     int   rtt_rtt;            /* most recent measured RTT, in seconds */
     int   rtt_srtt;           /* smoothed RTT estimator, in seconds */
     int   rtt_rttvar;         /* smoothed mean deviation, in seconds */
     int     rtt_rto;            /* current RTO to use, in seconds */
     int     rtt_nrexmt;         /* # times retransmitted: 0, 1, 2, ... */
-    // End
     uint32_t rtt_base;          /* # sec since 1/1/1970 at start */
 };
 
@@ -21,7 +18,7 @@ struct rtt_info {
 #define RTT_RXTMAX     3000      /* max retransmit timeout value, in seconds */
 #define RTT_MAXNREXMT  12       /* max # times to retransmit */
 
-                /* function prototypes */
+           
 void    rtt_debug(struct rtt_info *);
 void    rtt_init(struct rtt_info *);
 void    rtt_newpack(struct rtt_info *);
@@ -30,5 +27,5 @@ void    rtt_stop(struct rtt_info *, uint32_t);
 int     rtt_timeout(struct rtt_info *);
 uint32_t rtt_ts(struct rtt_info *);
 
-extern int rtt_d_flag;          /* can be set to nonzero for addl info */
+extern int rtt_d_flag;         
 #endif  /* __unp_rtt_h */
